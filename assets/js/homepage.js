300ba91bea204d5261e5d6b3fb7f1975362d082d
Michael Kendrick <michael@locumsmart.net>
2022-03-06 15:02:38 -0600

    issue 1

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -16,6 +16,10 @@ var formSubmitHandler = function (event) {
 };
 
 var displayRepos = function (repos, searchTerm) {
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
   repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
   for (var i = 0; i < repos.length; i++) {
@@ -23,8 +27,20 @@ var displayRepos = function (repos, searchTerm) {
     var repoEl = document.createElement("div");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
     var titleEl = document.createElement("span");
-    titleEl.textConent = repoName;
+    titleEl.textContent = repoName;
     repoEl.appendChild(titleEl);
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+    if (repos[i].open_issues_count > 0) {
+      statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" +
+        repos[i].open_issues_count +
+        " issue(s)";
+    } else {
+      statusEl.innerHTML =
+        "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+    repoEl.appendChild(statusEl);
     repoContainerEl.appendChild(repoEl);
   }
   console.log(repos);
@@ -33,11 +49,19 @@ var displayRepos = function (repos, searchTerm) {
 
 var getUserRepos = function (user) {
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
-  fetch(apiUrl).then(function (response) {
-    response.json().then(function (data) {
-      displayRepos(data, user);
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayRepos(data, user);
+        });
+      } else {
+        alert("Error: GitHub User Not Found");
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect with GitHub");
     });
-  });
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
